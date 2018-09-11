7b12dc1a53021dfb7e2b138b2275ae5f003e2374
George Chepiga <egorchepiga@gmail.com>
2018-09-11 13:07:34 +0300

    Top words functionality and bugfixes

--- stats-web/src/store/graphics/top/reducer.js
+++ stats-web/src/store/graphics/top/reducer.js
@@ -5,10 +5,19 @@ const initialState = { forChat: false};
 export default function topWordsForChat(state = initialState, action) {
     if (action.type === types.SET_SECOND_ALL) {
         return {
+            ...state,
             forChat : action.payload.forChat,
             options: action.payload.options,
             data: action.payload.data
         };
     }
+
+    if (action.type === types.LOAD_MORE_WORDS) {
+        return {
+            ...state,
+            more_user_words : action.payload
+        };
+    }
+
     return state;
 }

4b2204de0992d53cbfdd8c72098b7fd534cd26ae
George Chepiga <egorchepiga@gmail.com>
2018-09-11 13:13:49 +0300

    Userlist added

--- stats-web/src/containers/userlist.js
+++ stats-web/src/containers/userlist.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {setChosen} from '../store/getStats/chosen/action'
+import {createTopWordsForChat} from "../store/graphics/top/action";
+import {createTopStickers} from "../store/graphics/stickers_top/action";
+import {createTimeMessage} from "../store/graphics/time/action";
+import {loadUserWords} from "../store/graphics/top/action";
+import {findUser} from "../store/containers/userlist/action";
 
 class UserList extends Component {
 
@@ -7,20 +13,115 @@ class UserList extends Component {
         super(props);
     }
 
-    componentWillMount() {
-    }
+    chooseUser = (event) => {
+        this.props.chooseUser(event.target.id);
+
+        let users = this.props.store.chat.users;
+        let user, index;
+        for(let i=0; i<users.length; i++) {
+            if(users[i].user === event.target.id) {
+                console.log(users[i])
+                user = users[i];
+                index = i;
+                break;
+            }
+        }
+        console.log(user.user);
+        this.props.createTopWordsForUser(
+            this.props.store.chat,
+            index,
+            this.props.store.token,
+            this.props.store.chat.id,
+            user.id,
+            20,
+            user.user
+        );
+
+        this.props.createTopStickersForUser(this.props.store.chat, event.target.id);
+
+        this.props.createTimeMessageForUser(
+            this.props.store.timeMessage.RAWTime,
+            this.props.store.timeMessage.dayScale,
+            this.props.store.timeMessage.imposition,
+            this.props.store.timeMessage.fromTime,
+            this.props.store.timeMessage.toTime,
+            this.props.store.timeMessage.timeScale,
+            this.props.store.timeMessage.average,
+            this.props.store.timeMessage.periods,
+            true,
+            event.target.id,
+            this.props.store.chat.time
+        );
+
+    };
+
+    createUserProfile = (user, index) => (
+        <div key={index} onClick={this.chooseUser} id={user.user} className="user-preview">
+            <img  id={user.user} className="user-image" src={user.img ? 'https://egorchepiga.ru/tg-stats/' + user.img
+            : 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/1200px-Telegram_logo.svg.png'}/>
+            <label id={user.user} >{user.user + " #" + (index+1)}</label>
+        </div>
+    );
+
+    userList = (usersStore) => {
+        //let regExp = new RegExp('^[' + this.props.store.userList.find + ']*$');
+        let users = usersStore.slice();
+        let tmp = [];
+        while (users.length > 0 && tmp.length < 7) {
+            let user = users[0],
+                index = 0;
+            for (let i = 1; i < users.length; i++) {
+                if (user.summary < users[i].summary) {
+                    user = users[i];
+                    index = i;
+                }
+            }
+            if(users[index].user.indexOf(this.props.store.userList.find) !== -1)
+                tmp.push(users[index]);
+            users.splice(index, 1);
+        }
+        return tmp.map(this.createUserProfile)
+    };
+
+    findUser = (event) => {
+        this.props.find(event.target.value);
+    };
 
     render() {
         return (
             <div>
-            </div>)
+                <input onChange={this.findUser}></input>
+                <div className="user-list">
+                   {this.userList(this.props.store.chat.users)}
+                </div>
+            </div>
+        )
     }
 }
 
-
-
-export default connect(state => ({
+export default connect(
+    state => ({
         store: state
     }), dispatch => ({
-    });
+        chooseUser :  (chosen) => {
+            dispatch(setChosen(chosen))
+        },
+        createTopWordsForUser: (data, index, token, chat_id, user_id, count, chosen) => {
+            dispatch(loadUserWords({token, chat_id, user_id, count}))
+                .then(res => {
+                    console.log(data);
+                    data.users[index].top_words = res;
+                    dispatch(createTopWordsForChat(data, false, chosen));
+            })
+        },
+        createTopStickersForUser: (data, chosen) => {
+            dispatch(createTopStickers(data, false, chosen));
+        },
+        createTimeMessageForUser: (time, dayScale, imposition, fromTime, toTime, customScale, average, periods, messagesActivity, chosen, RAWTime) => {
+            dispatch(createTimeMessage(time, dayScale, imposition, fromTime, toTime, customScale, average, periods, messagesActivity, chosen, RAWTime))
+        },
+        find: (string) => {
+            dispatch(findUser(string));
+        }
+    })
 )(UserList)
